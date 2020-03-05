0f273fcbf06c21420613196683224852b0af7882
JabinWashburn <jabinwashburn@gmail.com>
2020-03-05 16:57:47 -0700

    finished the project

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,31 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
-
+function consume (a, b, cb){
+  return cb(a, b);
+}
+console.log(consume);
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (a, b) => {
+  return a + b;
+}
 
+const multiply = (a, b) => {
+  return a * b; 
+}
+
+const greeting = (a, b) => {
+  return `Hello ${a} ${b}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,17 +38,17 @@
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
-
+//because the child function is nested in the scope of the parent function it can access inside its parent. since internal was defined witin parent child has access becuse its in that scope. 
 
 const external = "I'm outside the function";
 
-function myFunction() {
+function myFunction() { // this is part of the parent function
   console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+  const internal = "Hello! I'm inside myFunction!"; //here is where internal was defined. 
 
-  function nestedFunction() {
+  function nestedFunction() {//here is the child
     console.log(internal);
-  };
+  }; //here is where child ends. 
   nestedFunction();
-}
+} // here is where parent function ends 
 myFunction();
